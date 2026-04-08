790c009dfefd5be2a7d6df255ce2d54ee1349ab4
alesya <a.davydova5@g.nsu.ru>
2026-04-08 14:35:12 +0700

    Update ex11.py

--- practicum11/ex11.py
+++ practicum11/ex11.py
@@ -6,8 +6,8 @@ comm2 = comm[1]
 shift = int(comm2) % len(nms)
 res = []
 if (comm1 == 'R') or (comm1 == 'r'):
-    res = (nms[-shift:] + nms[:-shift])
+    res = nms[-shift:] + nms[:-shift]
 elif (comm1 == 'L') or (comm1 == 'l'):
-    res = (nms[shift:] + nms[:shift])
+    res = nms[shift:] + nms[:shift]
 resint = [int(_) for _ in res]
 print(resint)
