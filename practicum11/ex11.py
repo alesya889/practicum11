d0cce22af2d74166c01066a9fb3907a4eb4dcf11
alesya <a.davydova5@g.nsu.ru>
2026-04-08 14:33:19 +0700

    Update ex11.py

--- practicum11/ex11.py
+++ practicum11/ex11.py
@@ -3,10 +3,11 @@ nms = numbs.split()
 comm = input('Введите комманду: ')
 comm1 = comm[0]
 comm2 = comm[1]
-res = []
 shift = int(comm2) % len(nms)
+res = []
 if (comm1 == 'R') or (comm1 == 'r'):
-    res.append(nms[-shift:] + nms[:-shift])
+    res = (nms[-shift:] + nms[:-shift])
 elif (comm1 == 'L') or (comm1 == 'l'):
-    res.append(nms[shift:] + nms[:shift])
-print(*res)
+    res = (nms[shift:] + nms[:shift])
+resint = [int(_) for _ in res]
+print(resint)
